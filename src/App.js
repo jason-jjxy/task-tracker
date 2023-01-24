687dbcfcefd2c778deb3699273e485adf4ef137b
Jason526 <jasy526@hotmail.com>
2023-01-23 21:12:34 -0500

    Added a toggle and removal feature

--- src/App.js
+++ src/App.js
@@ -13,13 +13,31 @@ const App = () => {
             text: "Food Shopping"
         }
     ])
+    // Delete Task
+    const deleteTask = (id) => {
+      setTasks(tasks.filter((task) => task.id !== id))
+    }
+
+    // Toggle Reminder 
+    const toggleReminder = (id) => {
+      setTasks(
+        tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task)
+      )
+    }
+
+
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks}/>
+      {tasks.length > 0 ? (
+        <Tasks  tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>
+      ): (
+        "No tasks to Show"
+      )}
     </div>
-  );
+  )
 }
+
 
 
 export default App;
